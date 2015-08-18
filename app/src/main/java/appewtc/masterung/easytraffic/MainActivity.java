52e201507fca493f4f3787afef1d364a3e5ac208
masterUNG <phrombutr@gmail.com>
2015-08-18 14:42:13 +0700

    Finish Inten

--- app/src/main/java/appewtc/masterung/easytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easytraffic/MainActivity.java
@@ -1,9 +1,12 @@
 package appewtc.masterung.easytraffic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -59,6 +62,18 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, strTitle);
         trafficListView.setAdapter(objMyAdapter);
+
+        //Active onClick on Listview
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                //Intent to detail
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(objIntent);
+
+            }   // event
+        });
 
 
     }   // createListView
