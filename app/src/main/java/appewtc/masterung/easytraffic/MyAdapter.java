eef283b9f18531613e862e7f47a6be7757320bdb
masterUNG <phrombutr@gmail.com>
2015-08-18 11:22:21 +0700

    Brfore Break

---
+++ app/src/main/java/appewtc/masterung/easytraffic/MyAdapter.java
@@ -0,0 +1,47 @@
+package appewtc.masterung.easytraffic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by masterUNG on 8/18/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }   // Constructor
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        
+
+        return null;
+    }   // getView
+}   // Main Class
