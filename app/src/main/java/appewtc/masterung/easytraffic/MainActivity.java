f481036a000236491f84519a31b6279908e4f11a
masterUNG <phrombutr@gmail.com>
2015-08-19 11:39:39 +0700

    ทดสอบ

--- app/src/main/java/appewtc/masterung/easytraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/easytraffic/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickTest(View view) {
         startActivity(new Intent(this, TestActivity.class));
-    }
+    }   // clickTest
 
 
     private void createListView() {
